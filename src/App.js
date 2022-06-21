b3f686e4dc5d3fdfc317796a5ae1a6f098bc845a
DESKTOP-MTDGO5G\AMIR <amiralianwar611@gmail.com>
2022-06-21 07:44:34 +0500

    Project completed

--- src/App.js
+++ src/App.js
@@ -2,7 +2,21 @@ import React, { useState } from 'react';
 import data from './data';
 import SingleQuestion from './Question';
 function App() {
-  return <h2>accordion project setup</h2>;
+  const [question,Setquestion]=useState(data)
+  return (
+    <main>
+      <div className='container'>
+      <h3>questions and answers about login</h3>
+      <section className='info'>
+      {
+        data?.map((item)=>(
+          <SingleQuestion question={item} key={item.id} />
+        ))
+      }
+      </section>
+      </div>
+    </main>
+  )
 }
 
 export default App;
--- src/Question.js
+++ src/Question.js
@@ -1,7 +1,19 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
-const Question = () => {
-  return <h2>question component</h2>;
+const Question = ({question}) => {
+ const [toggle, setToggle]= React.useState(false)
+ const ToggleHandler=()=>{
+  setToggle(!toggle)
+ }
+  return (
+    <article className='question'>
+    <header>
+      <h4>{question.title}</h4>
+      <button className='btn' onClick={ToggleHandler}>{!toggle ? <AiOutlinePlus />:<AiOutlineMinus />}</button>
+    </header>
+    {toggle &&<p>{question.info}</p>}  
+    </article>
+  )
 };
 
 export default Question;
